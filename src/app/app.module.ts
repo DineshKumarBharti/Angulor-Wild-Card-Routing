021f561b0c7d327c40187a01f6161468ade9f732
dinesh <dinesh.bharti.be16@gmail.com>
2023-11-17 05:56:09 -0800

    login form create and binding concept

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -20,7 +22,9 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
-
+export class LoginComponent{
+constructor(){}
+ngOnInit():void{}
+hello(studentlogin:NgForm):void{
+  console.log(studentlogin.value);
+}
 }
